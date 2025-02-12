c7b85c7fdef273a40adcbba9cb2a5f523888dd5e
fercarneir0 <dev.luisfernando12@gmail.com>
2025-02-11 23:43:10 -0300

    fix: Hasheando a senha do usuário

--- src/model/User.js
+++ src/model/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const autoIncrement = require('mongoose-sequence')(mongoose);
+const bcrypt = require('bcryptjs');
 
 
 // Definindo o schema do modelo do User
@@ -14,6 +15,19 @@ const UserSchema = new mongoose.Schema({
 
 //Incrementando o ID automaticamente
 UserSchema.plugin(autoIncrement, {inc_field: "userId"});
+
+//Hash da senha antes de salvar
+UserSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next();
+
+    try{
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err){
+        next(err);
+    }
+});
 
 //Criando o model do schema
 const User = mongoose.model('User', UserSchema);
