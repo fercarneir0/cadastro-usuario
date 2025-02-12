d73777ad773d20769e4d103a3ac7a803136db2c0
fercarneir0 <dev.luisfernando12@gmail.com>
2025-02-12 20:44:15 -0300

    fix: Adicionando o arquivo .env para as conexões

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require("mongoose");
 const userRoutes = require("./Routes/UserRoutes");
 
+const mongoUri = process.env.MONGO_URI;
+const port = process.env.PORT;
+
 const app = express();
 app.use(express.json());
 app.use("/api", userRoutes)
-const port = 3000;
-mongoose.connect('mongodb+srv://devluisfernando12:gustavopasquasafadinha@cadastro.yox3l.mongodb.net/?retryWrites=true&w=majority&appName=cadastro')
+mongoose.connect(mongoUri)
 
 app.listen(port, () => {
     console.log(`Aplicacao escutando na porta ${port}`)
